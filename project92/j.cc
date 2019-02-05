6ecdfd06c16deef3589ffa751a1e4a84361eae99
momoNorthy <northy360@126.com>
2019-02-05 13:16:43 +0800

    hello,world for windows

---
+++ project92/j.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+int wmain(int argc, const wchar_t* argv[])//windows version,\
+										  if you use linux or UNIX,please change it into\
+										 "int main(int argc,const char*argv[])"or int main().
+{
+	std::cout << "hello,world!\n";
+	return 0;//optional
+}
